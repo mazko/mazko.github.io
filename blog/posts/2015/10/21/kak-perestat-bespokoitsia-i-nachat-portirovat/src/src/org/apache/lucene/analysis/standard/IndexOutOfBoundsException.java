24ebee09d0213fce6f6171312d85b38018e96dfb
Docker CI <>
2025-03-11 10:29:50 +0000

    up

--- blog/posts/2015/10/21/kak-perestat-bespokoitsia-i-nachat-portirovat/src/src/org/apache/lucene/analysis/standard/IndexOutOfBoundsException.java
+++
@@ -1,6 +0,0 @@
-package org.apache.lucene.analysis.standard;
-
-@SuppressWarnings("serial")
-public class IndexOutOfBoundsException extends Exception {
-
-}
--- blog/posts/2015/10/21/kak-perestat-bespokoitsia-i-nachat-portirovat/src/tests/org/apache/lucene/analysis/standard/Slow.java
+++
@@ -1,5 +0,0 @@
-package org.apache.lucene.analysis.standard;
-
-public @interface Slow {
-
-}
